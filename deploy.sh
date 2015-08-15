a3696aa3f629af79b9e839468ee721a5dbb883ec
Yauheni Batsianouski <Yauheni_Batsianouski@epam.com>
2015-08-15 23:00:32 +0300

    123

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-ARTIFACT_FILE=$1
-
-echo $ARTIFACT_FILE
 cd ~/CI/task8/taskfolders
+
 rm -rf ./old/*
 echo "old artifact removed"
+
 mv ./current/*.tar.gz ./old/
 echo "currend aftifact saved into old"
+
 mv ./predeploy/*.tar.gz ./current/
 echo "new artifact saved"
+
+cd ./dist
+tar -zxvf ../current/*.tar.gz\
+echo "current artifact unpacking"
+
+rm -rf ~/custom/deploy/cdtask.war/*
+cp -r ./* ~/custom/deploy/cdtask.war/
+echo "new version copied into jboss application-folder"
+
+#twiddle.sh -s 192.168.122.16:1099 -u admin -p P@ssword invoke 'jboss.service:MainDeployer' redeploy "file:///home/Student/custom/deploy/cdtask.war"
