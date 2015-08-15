f04b10a50ce7d7172f2499a4536255fb0cd35bdb
Yauheni Batsianouski <Yauheni_Batsianouski@epam.com>
2015-08-15 22:30:47 +0300

    123

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-ARIFACT_FILE=$1
+ARTIFACT_FILE=$1
 
 echo $ARTIFACT_FILE
 cd ~/CI/task8/taskfolders
-rm -rf ./old/*.tar.gz
+rm -rf ./old/*
 echo "old artifact removed"
-mv ./current/$ARIFACT_FILE ./old
+mv "./current/$ARTIFACT_FILE" ./old/
 echo "currend aftifact saved into old"
-mv ./predeploy/$ARTIFACT_FILE ./current
+mv "./predeploy/$ARTIFACT_FILE" ./current/
 echo "new artifact saved"
