400b48792f3620a6a0734e2c772e90e8bcc8f098
Yauheni Batsianouski <Yauheni_Batsianouski@epam.com>
2015-08-15 23:34:33 +0300

    add checking

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 cd ~/CI/task8/taskfolders
 
 rm -rf ./old/*
+rm -tf ./dist/*
 echo "old artifact removed"
 
 mv ./current/*.tar.gz ./old/
@@ -12,11 +13,21 @@ mv ./predeploy/*.tar.gz ./current/
 echo "new artifact saved"
 
 cd ./dist
-tar -zxvf ./current/*.tar.gz
+tar -zxvf ../current/*.tar.gz
 echo "current artifact unpacking"
 
 rm -rf ~/custom/deploy/cdtask.war/*
-cp -r ./* ~/custom/deploy/cdtask.war/
+cp -r ./app/* ~/custom/deploy/cdtask.war/
 echo "new version copied into jboss application-folder"
 
 #twiddle.sh -s 192.168.122.16:1099 -u admin -p P@ssword invoke 'jboss.service:MainDeployer' redeploy "file:///home/Student/custom/deploy/cdtask.war"
+
+echo "
+	checking deploy
+"
+if [ $(curl -s -I http://192.168.122.16:9090/cdtask/  | grep HTTP/1.1 | awk {'print $2'}) == "200" ]
+then
+	echo "success!"
+else
+	echo "rollback"
+fi
