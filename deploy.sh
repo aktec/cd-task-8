d156da0e8ab0f27ee908806b34e484f803621bb0
Yauheni Batsianouski <Yauheni_Batsianouski@epam.com>
2015-08-15 23:50:44 +0300

    second version

--- deploy.sh
+++ deploy.sh
@@ -22,6 +22,9 @@ echo "new version copied into jboss application-folder"
 
 #twiddle.sh -s 192.168.122.16:1099 -u admin -p P@ssword invoke 'jboss.service:MainDeployer' redeploy "file:///home/Student/custom/deploy/cdtask.war"
 
+#Getting to jboss some time for redeploying application
+sleep 60
+
 echo "checking deploy"
 if [ $(curl -s -I -L http://192.168.122.16:9090/cdtask/  | grep HTTP/1.1 | awk {'print $2'}) == "200" ]
 then
