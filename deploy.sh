b46948aacfff55d5f38d605ab3ea7f977f021b43
Yauheni Batsianouski <Yauheni_Batsianouski@epam.com>
2015-08-16 00:08:57 +0300

    broken version. rollback.sh v0.2

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ~/CI/task8/taskfolders
+cd /home/Student/CI/task8/taskfolders
 
 rm -rf ./old/*
 rm -rf ./dist/*
@@ -22,32 +22,13 @@ echo "new version copied into jboss application-folder"
 
 #twiddle.sh -s 192.168.122.16:1099 -u admin -p P@ssword invoke 'jboss.service:MainDeployer' redeploy "file:///home/Student/custom/deploy/cdtask.war"
 
-#Getting to jboss some time for redeploying application
-sleep 60
-
 echo "checking deploy"
+#Getting to jboss some time for redeploying application
+sleep 30
+
 if [ $(curl -s -I -L http://192.168.122.16:9090/cdtask/  | grep HTTP/1.1 | awk {'print $2'}) == "200" ]
 then
 	echo "success!"
 else
-	echo "SHIT! sollback"
-	
-	cd ~/CI/task8/taskfolders
-
-	mv ./old/*.tar.gz ./current/
-	echo "old aftifact restored"
-
-	cd ./dist
-	tar -zxvf ../old/*.tar.gz
-	echo "old artifact unpacking"
-
-	rm -rf ~/custom/deploy/cdtask.war/*
-	cp -r ./app/* ~/custom/deploy/cdtask.war/
-	echo "old version copied into jboss application-folder"
-
-	if [ $(curl -s -I -L http://192.168.122.16:9090/cdtask/  | grep HTTP/1.1 | awk {'print $2'}) == "200" ]
-	then
-		echo "Old version had been restored successfully"
-	fi
-
+	bash ./rollback.sh	
 fi
---
+++ rollback.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo "SHIT! sollback"
+    
+cd /home/Student/CI/task8/taskfolders
+	
+rm -rf ./current/*
+	
+mv ./old/*.tar.gz ./current/
+echo "old aftifact restored"
+
+cd ./dist
+tar -zxvf ../current/*.tar.gz
+echo "old artifact unpacking"
+
+rm -rf ~/custom/deploy/cdtask.war/*
+cp -r ./app/* ~/custom/deploy/cdtask.war/
+echo "old version copied into jboss application-folder"
+
+echo "Checking old version"
+sleep 30
+
+if [ $(curl -s -I -L http://192.168.122.16:9090/cdtask/  | grep HTTP/1.1 | awk {'print $2'}) == "200" ]
+then
+    echo "Old version had been restored successfully"
+else
+    echo "Something goes wrong"
+fi
+
